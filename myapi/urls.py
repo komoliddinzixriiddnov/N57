84434406b44bd2614633bd9e282e77bbe6fbbd97
komoliddinzixriiddnov <153748320+komoliddinzixriiddnov@users.noreply.github.com>
2025-02-28 14:29:59 +0500

    Update urls.py

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, MovieViewSet, ActorViewSet, CommentViewSet
+
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'movies', MovieViewSet)
-router.register(r'actors', ActorViewSet)
-router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
